06727d9009edc67c5d7169cb74e67db63bc2bc51
Neosporin10 <mcritzer10@gmail.com>
2020-04-19 22:42:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-java-raptor
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-java-raptor)
---
+++ index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Mikey Raptor'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          Blogging while making progressive web apps!
+        </p>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
